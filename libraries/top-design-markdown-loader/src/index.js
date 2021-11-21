027f926761f3027a8bbcbdbfc9edd6460da08597
smallwebbird <1175543548@qq.com>
2021-11-21 16:27:32 +0800

    feat: 123

--- libraries/top-design-markdown-loader/src/index.js
+++ libraries/top-design-markdown-loader/src/index.js
@@ -115,7 +115,7 @@ module.exports = function (source) {
     import React from 'react';
     import { DemoCodePreview  } from '@top-design/components';
   `);
-  console.log(4677444444442);
+  console.log(42);
   const returnSource = `
     export default function () {
       return <div className="td-document">
