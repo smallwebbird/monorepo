86e7c67a2619eb7b5035bd571bf28b0404a8c7ed
smallwebbird <1175543548@qq.com>
2021-11-21 22:34:13 +0800

    feat: 增加lint

--- common/autoinstallers/rush-lint/scripts/lint.js
+++ common/autoinstallers/rush-lint/scripts/lint.js
@@ -1,13 +1,24 @@
 const path = require('path');
-const execShell = require('child_process').execSync;
+const fs = require('fs');
+const execa = require('execa');
 
 const gitPath = path.resolve(__dirname, '../../../../.git');
+const configPath = path.resolve(__dirname, './commitlint.config.js');
 const commitlintBinPath = path.resolve(__dirname, '../node_modules/.bin/commitlint');
-console.log(commitlintBinPath);
-const commitlintConfig = path.resolve(__dirname, './commitlint.config.js');
 
-try {
-  execShell(`"${commitlintBinPath}"`, { stdio: 'inherit' });
-} catch (e) {
-  console.log(e);
+if (!fs.existsSync(gitPath)) {
+    console.error('no valid .git path');
+    process.exit(1);
+}
+
+main();
+
+async function main() {
+    try {
+        await execa('bash', [commitlintBinPath, '--config', configPath, '--cwd', path.dirname(gitPath), '--edit'], {
+            stdio: 'inherit',
+        });
+    } catch (e) {
+        process.exit(1);
+    }
 }
