c11c45fe46a85b4d462c7b7484319cd34b95c933
smallwebbird <1175543548@qq.com>
2021-11-21 23:44:39 +0800

    feat: 提交

--- common/autoinstallers/rush-lint/scripts/lint.js
+++ common/autoinstallers/rush-lint/scripts/lint.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const execa = require('execa');
-const execFile = require('child_process').execFile;
 
 const gitPath = path.resolve(__dirname, '../../../../.git');
 const configPath = path.resolve(__dirname, './commitlint.config.js');
@@ -11,21 +10,14 @@ if (!fs.existsSync(gitPath)) {
     console.error('no valid .git path');
     process.exit(1);
 }
-console.log(process.cwd(), __dirname);
-const child = execFile(commitlintBinPath, ['--version'], (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
-// main();
+main();
 
-// async function main() {
-//     try {
-//         await execa('bash', [commitlintBinPath, '--config', configPath, '--cwd', path.dirname(gitPath), '--edit'], {
-//             stdio: 'inherit',
-//         });
-//     } catch (e) {
-//         process.exit(1);
-//     }
-// }
+async function main() {
+    try {
+        await execa('bash', [commitlintBinPath, '--config', configPath, '--cwd', path.dirname(gitPath), '--edit'], {
+            stdio: 'inherit',
+        });
+    } catch (e) {
+        process.exit(1);
+    }
+}
