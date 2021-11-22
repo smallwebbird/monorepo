35da95efd2be69b4872da2228ab4bc96f934a7fd
smallwebbird <1175543548@qq.com>
2021-11-23 00:32:22 +0800

    123

--- common/autoinstallers/rush-lint/scripts/lint.js
+++ common/autoinstallers/rush-lint/scripts/lint.js
@@ -1,31 +1,20 @@
 const path = require('path');
 const fs = require('fs');
-const execa = require('execa');
 const { spawnSync } = require('child_process');
 
 const gitPath = path.resolve(__dirname, '../../../../.git');
 const configPath = path.resolve(__dirname, './commitlint.config.js');
 const commitlintBinPath = path.resolve(__dirname, '../node_modules/.bin/commitlint');
 
+if (!fs.existsSync(gitPath)) {
+    console.error('no valid .git path');
+    process.exit(1);
+}
+
  try {
-  spawnSync(`${commitlintBinPath} --edit --config ${configPath}`, { shell: true, stdio: 'inherit' })
+  spawnSync(`${commitlintBinPath} --edit --config ${configPath} --cwd ${path.dirname(gitPath)}`, { shell: true, stdio: 'inherit' })
  } catch(e){
-   console.log(e)
+   console.log(e);
+   process.exit(1);
  }
 
-
-// if (!fs.existsSync(gitPath)) {
-//     console.error('no valid .git path');
-//     process.exit(1);
-// }
-// main();
-
-// async function main() {
-//     try {
-//         await execa('bash', [commitlintBinPath, '--config', configPath, '--cwd', path.dirname(gitPath), '--edit'], {
-//             stdio: 'inherit',
-//         });
-//     } catch (e) {
-//         process.exit(1);
-//     }
-// }
