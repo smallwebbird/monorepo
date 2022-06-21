924a41e1f7cd3980b4dff2101024770ecca9ac8f
smallwebbird <1175543548@qq.com>
2022-06-22 00:08:06 +0800

    feat: 提交代码

--- libraries/top-design-markdown-loader/src/index.js
+++ libraries/top-design-markdown-loader/src/index.js
@@ -115,7 +115,7 @@ module.exports = function (source) {
     import React from 'react';
     import { DemoCodePreview  } from '@top-design/components';
   `);
-  console.log(444993332222);
+  console.log(444993335552222);
   const returnSource = `
     export default function () {
       return <div className="td-document">
